7c973636c8781c4f2bfe260ca70721ce19095bd1
pradeep <16036792+npradeep357@users.noreply.github.com>
2023-10-08 01:13:39 +0530

    Create __main__.py

---
+++ server/304/__main__.py
@@ -0,0 +1,65 @@
+import argparse
+import logging
+import logging.config
+import time
+
+from .app import App
+
+from argparse import ArgumentParser
+from logging.handlers import TimedRotatingFileHandler
+
+
+def _add_arguments(parser: ArgumentParser):
+    parser.description = "304-The-Game"
+    parser.add_argument("--host", default="127.0.0.1", help="Bind to this address")
+    parser.add_argument("--port", type=int, default=3040, help="Bind to this port")
+    parser.add_argument("--version", default="SNAPSHOT", help="Version of the project")
+    parser.add_argument("--log-level", default="info", help="Log Level")
+    parser.add_argument("--log-file", default="game.log")
+
+
+LOG_FORMAT = "%(asctime)s {0} - %(levelname)s - %(name)s - %(message)s".format(
+    time.localtime().tm_zone
+)
+
+log_config = None
+
+
+def _configure_logger(log_level: str, log_file: str):
+    formatter = logging.Formatter(LOG_FORMAT)
+    log_handler = logging.StreamHandler()
+    log_handler.setFormatter(formatter)
+
+    # file_handler = logging.FileHandler(filename=log_file, encoding="utf-8")
+    file_handler = TimedRotatingFileHandler(
+        filename=log_file, encoding="utf-8", when="d", backupCount=10, interval=1
+    )
+
+    processing_logger = logging.getLogger("processing")
+
+    if log_level.upper() == "TRACE":
+        level = logging.NOTSET
+    elif log_level.upper() == "WARN":
+        level = logging.WARNING
+    elif log_level.upper() == "DEBUG":
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+
+    processing_logger.addHandler(log_handler)
+    processing_logger.addHandler(file_handler)
+    processing_logger.setLevel(level)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    _add_arguments(parser=parser)
+    args = parser.parse_args()
+    _configure_logger(args.log_level, args.log_file)
+
+    # runs indefinitively untill stopped.
+    App(args.host, args.port, args.version, args.log_level)
+
+
+if __name__ == "__main__":
+    main()
